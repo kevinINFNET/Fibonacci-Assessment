be144552ba20a90f51cd5e6489d28658c5dc73de
kevin <kevin@XDXDXD>
2024-06-21 13:58:03 -0300

    Adicionar arquivos de projeto.

---
+++ exec1/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{ static void Main()
+    {
+  Console.WriteLine("Digite a quantidade de números da sequência de Fibonacci que deseja mostrar:");
+   int n = int.Parse(Console.ReadLine());
+   if (n <= 0)
+ {
+   Console.WriteLine("Por favor, digite um número maior que zero.");
+   return;
+}
+PrintFibonacciSequence(n);
+} static void PrintFibonacciSequence(int n)
+{
+  int a = 0, b = 1;
+   for (int i = 0; i < n; i++)
+ {
+Console.Write(a + " ");
+  int next = a + b;
+  a = b;
+  b = next;
+}
+Console.WriteLine(); 
+ }
+}
